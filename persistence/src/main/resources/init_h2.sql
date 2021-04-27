09528cc74fc349738a41378781359fcbefc07df7
Andrey Belik <belikandrey01@gmail.com >
2021-04-27 16:25:05 +0300

    Edit db configuration(profiles)

--- persistence/src/main/resources/init_h2.sql
+++ persistence/src/main/resources/init_h2.sql
@@ -1,32 +1,3 @@
-SET MODE MySQL;
-
-create table tag(
-                    id int not null primary key auto_increment,
-                    name varchar(50) not null unique ,
-                    index tag_name_ind(name)
-);
-
-create table certificate(
-                            id int not null primary key auto_increment,
-                            name varchar(50) not null ,
-                            description varchar(150),
-                            price double not null ,
-                            duration int not null ,
-                            create_date datetime not null ,
-                            last_update_date datetime not null ,
-                            index certificate_name_ind(name)
-);
-
-
-create table certificate_tag(
-                                certificate_id int not null ,
-                                tag_id int not null ,
-                                foreign key (certificate_id)
-                                    references certificate(id) on update cascade ,
-                                foreign key (tag_id)
-                                    references tag(id) on update cascade ,
-                                primary key (certificate_id, tag_id)
-);
 
 
 insert into tag(name) values ('#cool');
@@ -79,9 +50,7 @@ insert into certificate_tag(certificate_id, tag_id) values(7,4);
 insert into certificate_tag(certificate_id, tag_id) values(7,2);
 insert into certificate_tag(certificate_id, tag_id) values(7,8);
 
-/////////
-
-new script
+/*
 drop table user;
 drop table user_order;
 
@@ -141,6 +110,6 @@ values (4, 5);
 
 insert into order_certificate(order_id, certificate_id)
 values (5, 7);
-
+*/
 
 
--- web/src/main/java/com/epam/esm/SpringBootIntroApplication.java
+++ web/src/main/java/com/epam/esm/SpringBootIntroApplication.java
@@ -1,10 +1,7 @@
 package com.epam.esm;
 
-import com.epam.esm.config.PersistenceSpringConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class SpringBootIntroApplication {
